ac93d6f8edefaefcae27473635d999415fef3698
xiapeng <xiaxianpeng@autohome.com.cn>
2024-08-09 19:49:08 +0800

    时间校验

--- src/main/java/com/example/springwebgateway/config/SignVerifyFilter.java
+++ src/main/java/com/example/springwebgateway/config/SignVerifyFilter.java
@@ -1,6 +1,5 @@
 package com.example.springwebgateway.config;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import javax.servlet.FilterChain;
@@ -8,8 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,17 +21,32 @@ public class SignVerifyFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (request instanceof ContentCachingRequestWrapper) {
             ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
-            boolean isValid;
-            // 检查请求体的签名
-            isValid = this.verifySignature(cachingRequest);
-            // 如果签名无效，返回错误响应
-            if (!isValid) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.getWriter().write("Invalid signature");
+            // 如果时间戳无效，返回错误响应
+            if (!this.verifyTimestamp(request.getHeader("x-timestamp"))) {
+                writeResponse(response, "Invalid timestamp");
                 return;
             }
+            // 如果签名无效，返回错误响应
+            if (!this.verifySignature(cachingRequest)) {
+                writeResponse(response, "Invalid signature");
+                return;
+            }
+            // todo 多次请求
+            // 继续处理请求
             filterChain.doFilter(cachingRequest, response);
         }
+    }
+
+    private static void writeResponse(HttpServletResponse response, String content) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(content);
+    }
+
+    private boolean verifyTimestamp(String timestamp) {
+        if (Math.abs(System.currentTimeMillis() - Long.parseLong(timestamp)) > 1000 * 60 * 60) {
+            return false;
+        }
+        return true;
     }
 
     private boolean verifySignature(ContentCachingRequestWrapper request) {
@@ -62,21 +76,25 @@ public class SignVerifyFilter extends OncePerRequestFilter {
         return appKey;
     }
 
-    private String getRequestBody(HttpServletRequest request) {
-
-        if ("GET".equalsIgnoreCase(request.getMethod())) {
+    private static String getRequestBody(ContentCachingRequestWrapper request) {
+        // 对于GET请求，返回查询字符串
+        if (HttpMethod.GET.name().equalsIgnoreCase(request.getMethod())) {
             return request.getQueryString();
         }
-        if ("POST".equalsIgnoreCase(request.getMethod())) {
+        // 对于POST请求，检查是否为Multipart请求，以及是否有缓存的请求体内容
+        if (HttpMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
             if (request instanceof MultipartHttpServletRequest) {
+                // Multipart请求可能包含文件上传，不应该读取请求体作为字符串
                 return "";
-            }
-            try (BufferedReader reader = request.getReader()) {
-                return IOUtils.toString(reader);
-            } catch (Exception e) {
-                return "";
+            } else {
+                // 获取缓存的请求体
+                byte[] content = request.getContentAsByteArray();
+                if (content.length > 0) {
+                    return new String(content, StandardCharsets.UTF_8);
+                }
             }
         }
+        // 如果请求方法不是GET或POST，或者没有请求内容，则返回空字符串
         return "";
     }
 }
--- src/main/java/com/example/springwebgateway/config/WebMvcConfig.java
+++ src/main/java/com/example/springwebgateway/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.springwebgateway.config;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,7 +29,7 @@ public class WebMvcConfig {
         // 可以指定需要缓存请求的URL模式
         filterRegBean.addUrlPatterns("/*");
         // 设置过滤器优先级
-        filterRegBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        filterRegBean.setOrder(NumberUtils.INTEGER_ZERO);
         return filterRegBean;
     }
 }
